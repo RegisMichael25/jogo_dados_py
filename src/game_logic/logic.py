393add674d75e2e96e450855a3bf09d11c83a040
RegisMichael25 <regis.michael101@gmail.com>
2025-09-27 16:12:09 -0300

    Estagio 1 em progresso

--- src/game_logic/logic.py
+++ src/game_logic/logic.py
@@ -1,5 +1,42 @@
-#Craps:
+#ESTAGIO 1
+credito_inicial = 100
+valor_deposito = 0
+saldo_aposta = credito_inicial
 
-#Batalha com Dados:
+lista_de_jogadas = [0, 1, 1, 0, 1]
 
-#General (ou Bozó):
+def debito(saldo, percentual):
+    print("==========================================")
+    valor_debito = saldo * (percentual/100)
+    saldo -= valor_debito           
+    return saldo, valor_debito
+
+def credito(saldo, percentual):
+    print("==========================================")
+    valor_credito = saldo * (percentual/100)            
+    saldo += valor_credito
+    return saldo, valor_credito
+
+for jogadas in lista_de_jogadas:
+        if jogadas == 0:
+            print(f"saldo antes da aposta: {saldo_aposta:.2f}")
+            saldo_aposta, valor_perdido = debito(saldo_aposta, 30           )
+            print(f"Você perdeu {valor_perdido:.2f} na aposta! Seu saldo atual é {saldo_aposta:.2f}.")
+            print(f"saldo depois da aposta: {saldo_aposta:.2f}")
+        else:
+            print(f"saldo antes da aposta: {saldo_aposta:.2f}")
+            saldo_aposta, valor_ganho = credito(saldo_aposta, 10)
+            print(f"Você ganhou {valor_ganho:.2f} na aposta! Seu saldo atual é {saldo_aposta:.2f}.")
+            print(f"saldo depois da aposta: {saldo_aposta:.2f}")
+
+#ESTAGIO 2
+#valor_deposito = input("Digite o valor da aposta: ")
+#deposito = bool(valor_deposito)
+#if deposito is True:
+#    saldo_aposta += valor_deposito
+#    fichas = 5
+#else:
+#    fichas = 0
+
+
+#ESTAGIO 3
--- src/sicbo/sicbo.py
+++ src/sicbo/sicbo.py
@@ -1,20 +1,44 @@
 #Sic Bo:
 import random
-numeros = [random.randint(1, 6) for _ in range(3)]
+num = [random.randint(1, 6) for _ in range(3)]
 
-palpite = input("Qual é o seu palpite para a soma dos três dados? ")
+print("Bem-vindo ao Sic Bo!")
+print("Escolha qual modo de jogo deseja jogar:")
+print("1. Adivinhar a soma dos três dados")
+print("2. Adivinhar se um dos dados será um número específico (1-6)")
+print("3.Sair")
 
-for _ in numeros:
-    if palpite.isdigit() and int(palpite) in numeros:
+mode = input("Digite o número do modo de jogo que deseja jogar: ")
+if mode not in ['1', '2', '3']:
+    print("Escolha inválida. Tente novamente.")
+    exit()
+
+def game_mode(mode):
+    match mode:
+        case '1':
+            guess_sum()
+        case '2':
+            guess_specific()
+        case '3':
+            print("Obrigado por jogar! Até a próxima.")
+            exit()
+
+def guess_sum():
+    print("Tente adivinhar a soma dos três dados (entre 3 e 18).")
+    palpite = input("Qual é o seu palpite para a soma dos três dados? ")
+    if palpite.isdigit() and int(palpite) == sum(num):
         print("Parabéns! Você acertou o palpite.")
-        break
     else:
         print("Que pena! Você errou o palpite.")
 
-print("Números sorteados:", numeros)
-print("Soma dos números:", sum(numeros))
+def guess_specific():
+    palpite = input("Qual é o seu palpite para o número de um dos três dados? ")
+    if palpite.isdigit() and int(palpite) in num:
+        print("Parabéns! Você acertou o palpite.")
+        
+    else:
+        print("Que pena! Você errou o palpite.")
 
-if palpite.isdigit() and int(palpite) == sum(numeros):
-    print("Parabéns! Você acertou o palpite.")
-else:
-    print("Que pena! Você errou o palpite.")
+print("Números sorteados:", num)
+
+game_mode(mode)
